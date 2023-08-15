2a2bc3b28c17dc556944df8bec7fd557192942e2
T4-HADES <s3870730@rmit.edu.vn>
2023-08-15 16:16:07 +0700

    Initial Commit

---
+++ MonopolyGame/MonopolyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonopolyGameApp.swift
+//  MonopolyGame
+//
+//  Created by Thuc on 15/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct MonopolyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
